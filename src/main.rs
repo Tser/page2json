88f50827883da05199ca427b9f0ef4c61241b320
xiaobaiTser <807447312@qq.com>
2024-01-22 01:07:55 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// use reqwest::{ blocking::Client, Error};
+use reqwest;
+
+
+fn main(){
+
+    let response = reqwest::blocking::get("https://www.python.org/ftp/python/").unwrap().text();
+
+    // assert_eq!(response.status(), reqwest::StatusCode::OK);
+
+
+
+    println!("响应数据：\n{:#?}", response);
+
+    // Ok(());
 }
